1c3b380cb4a8baab657eb49de8d0835949a4e986
Jeremi Stadler <stadler.jeremi@gmail.com>
2025-03-03 12:28:56 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,13 @@ echo "Building...";
 go build -o ./build/clickhouse-udp;
 
 echo "Copying to server...";
-scp ./build/clickhouse-udp questdb:/home/ec2-user/clickhouse-udp.temp;
+scp ./build/clickhouse-udp clickhouse:/home/ec2-user/clickhouse-udp.temp;
 
 echo "Stopping...";
-ssh questdb "sudo systemctl stop clickhouse-udp.service"
+ssh clickhouse "sudo systemctl stop clickhouse-udp.service"
 
 echo "Renaming";
-ssh questdb "rm /home/ec2-user/clickhouse-udp; mv /home/ec2-user/clickhouse-udp.temp /home/ec2-user/clickhouse-udp"
+ssh clickhouse "rm /home/ec2-user/clickhouse-udp; mv /home/ec2-user/clickhouse-udp.temp /home/ec2-user/clickhouse-udp"
 
 echo "Starting";
-ssh questdb "sudo systemctl restart clickhouse-udp.service"
+ssh clickhouse "sudo systemctl restart clickhouse-udp.service"
